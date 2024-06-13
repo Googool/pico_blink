23250ee2eb657963f8bb0c90225b7e5099057a57
Ask Marius Lysgaard <askx0113@gmail.com>
2024-06-13 09:44:51 +0200

    Removed and renamed some functions

--- src/blink.c
+++ src/blink.c
@@ -10,22 +10,11 @@ void blink_toggle(uint pin) {
 }
 
 void blink_led(uint pin, uint delay_ms) {
-  gpio_put(pin, 1);
-  sleep_ms(delay_ms);
-  gpio_put(pin, 0);
+  blink_toggle(pin);
   sleep_ms(delay_ms);
 }
 
-void blink_led_times(uint pin, uint delay_ms, uint times) {
-  for (uint i = 0; i < times; i++) {
-    gpio_put(pin, 1);
-    sleep_ms(delay_ms);
-    gpio_put(pin, 0);
-    sleep_ms(delay_ms);
-  }
-}
-
-void blink_pulsate(uint pin, uint pulse_duration_ms) {
+void blink_led_pulse(uint pin, uint pulse_duration_ms) {
     gpio_set_function(pin, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(pin);
     pwm_config config = pwm_get_default_config();
@@ -36,19 +25,4 @@ void blink_pulsate(uint pin, uint pulse_duration_ms) {
       pwm_set_gpio_level(pin, i % 256);
       sleep_us(10);
     }
-}
-
-void blink_variable_speed(uint pin, uint min_delay_ms, uint max_delay_ms, uint step_ms) {
-  for (uint delay = min_delay_ms; delay <= max_delay_ms; delay += step_ms) {
-    gpio_put(pin, 1);
-    sleep_ms(delay);
-    gpio_put(pin, 0);
-    sleep_ms(delay);
-  }
-  for (uint delay = max_delay_ms; delay >= min_delay_ms; delay -= step_ms) {
-    gpio_put(pin, 1);
-    sleep_ms(delay);
-    gpio_put(pin, 0);
-    sleep_ms(delay);
-  }
 }
--- src/include/pico/blink.h
+++ src/include/pico/blink.h
@@ -5,8 +5,6 @@
 
 void blink_toggle(uint pin);
 void blink_led(uint pin, uint delay_ms);
-void blink_led_times(uint pin, uint delay_ms, uint times);
-void blink_pulsate(uint pin, uint pulse_duration_ms);
-void blink_variable_speed(uint pin, uint min_delay_ms, uint max_delay_ms, uint step_ms);
+void blink_led_pulse(uint pin, uint pulse_duration_ms);
 
 #endif // BLINK_H
