5cd865c6a2d30f903e2ff134ca3605093c5436b1
Ask Marius Lysgaard <askx0113@gmail.com>
2024-06-13 11:10:15 +0200

    Updated the blink_led_pulse function

--- src/blink.c
+++ src/blink.c
@@ -17,18 +17,21 @@ void blink_led(uint pin, uint delay_ms) {
 void blink_led_pulse(uint pin, uint pulse_duration_ms) {
   gpio_set_function(pin, GPIO_FUNC_PWM);
   uint slice_num = pwm_gpio_to_slice_num(pin);
+  uint channel = pwm_gpio_to_channel(pin);
+
   pwm_config config = pwm_get_default_config();
   pwm_config_set_clkdiv(&config, 4.0f);
   pwm_init(slice_num, &config, true);
 
-  uint step_size = 10;
+  uint max_brightness = 255;
+  uint step_size = pulse_duration_ms * 1000 / (2 * max_brightness);
 
-  for (uint i = 0; i <= 255; ++i) {
+  for (uint i = 0; i <= max_brightness; ++i) {
     pwm_set_gpio_level(pin, i);
     sleep_us(step_size);
   }
 
-  for (int i = 255; i >= 0; --i) {
+  for (int i = max_brightness; i >= 0; --i) {
     pwm_set_gpio_level(pin, i);
     sleep_us(step_size);
   }
